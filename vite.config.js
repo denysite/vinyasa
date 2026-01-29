d556f1f33e1800f32c6f9667f761c0ca16ef210a
Deny <boss.chudnivets@gmail.com>
2026-01-29 18:44:10 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import handlebars from "@vituum/vite-plugin-handlebars";
+import postcss from "@vituum/vite-plugin-postcss";
+import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import vituum from "vituum";
+
+export default defineConfig({
+  plugins: [
+    vituum(),
+    handlebars({
+      partials: {
+        directory: "src/components",
+        extname: false,
+      },
+    }),
+    postcss({
+      autoprefixer: {
+        overrideBrowserslist: ["last 2 versions", "> 1%"],
+      },
+    }),
+    ViteImageOptimizer({}),
+  ],
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: (source, filename) => {
+          // щоб не було рекурсії: vars.scss не має підключати сам себе
+          if (filename.replaceAll("\\", "/").endsWith("/src/scss/vars.scss")) {
+            return source;
+          }
+          return `@use "/src/scss/vars.scss" as *;\n${source}`;
+        },
+      },
+    },
+  },
+});
