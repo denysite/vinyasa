3fa7bb3bdf2e9f757a62de63e45ddbfe8af91a47
denysite <boss.chudnivets@gmail.com>
2026-01-30 12:19:22 +0200

    finished footer

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,13 @@
-const burgerButton = document.querySelector(".header__burger");
+const burgerOpenButton = document.querySelector(".header__burger");
+const burgerCloseButton = document.querySelector(".header__mobileMenu-close");
 
 document.addEventListener("click", (e) => {
-  if (e.target === burgerButton || burgerButton.contains(e.target)) {
-    document.body.classList.toggle("menu-opened");
+  if (e.target === burgerOpenButton || burgerOpenButton.contains(e.target)) {
+    document.body.classList.add("menu-opened");
+  } else if (
+    e.target === burgerCloseButton ||
+    burgerCloseButton.contains(e.target)
+  ) {
+    document.body.classList.remove("menu-opened");
   }
 });
